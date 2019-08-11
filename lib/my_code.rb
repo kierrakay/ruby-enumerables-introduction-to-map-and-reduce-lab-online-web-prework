c2774c0dc18893dbe165818121f21c71ce9b46f4
Kierra M <k.merriman32@hotmail.com>
2019-08-11 04:31:28 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
 # My Code here....
+def map_negativize_array(source_array)
+new_array = []
+counter = 0
+while counter < source_array.length do
+ new_array << source_array[counter] * -1 #**squaring incrments
+counter += 1 #increments plus 1 after squaring elements
+end
+return new_array #can also just write new_array without the retun before it 
+end
